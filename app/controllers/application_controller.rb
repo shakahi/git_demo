6a8e3d99f08f2b1b8a6374918f84a1b02221c8ea
Gerald Song <gerald.song@dansolutions.com>
2021-01-21 08:49:15 -1000

    Commit 1.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   #ADDED THIS LINE.
   #ADDED THIS LINE FROM MASTER 2.
+  #ADDED THIS LINE FROM FEATURE 2.
+     #ANOTHER LINE FROM FEATURE 2.
 end
