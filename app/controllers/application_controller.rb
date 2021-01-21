e243d0b4cda6ed11798b0f99f39e574d72dd816e
Gerald Song <gerald.song@dansolutions.com>
2021-01-20 14:01:12 -1000

    Commit 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   #ADDED THIS LINE.
-  #ADDED THIS LINE AT MASTER.
+  #ADDED THIS LINE FROM MASTER 2.
 end
